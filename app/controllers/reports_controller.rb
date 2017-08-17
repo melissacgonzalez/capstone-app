a254ffe11003923ee82bec6faf57b57739d5ca2c
Melissa Gonzalez <marvelissa@gmail.com>
2017-08-16 19:34:38 -0500

    Build out Update action on Reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,7 +11,7 @@ class ReportsController < ApplicationController
   end
 
   def new
-    @report = Report.create
+    @report = Report.new(overall_rating: 5)
     @events = current_user.events.where("datetime < ?", "now()").order(:datetime => "desc")
     render "new.html.erb"
   end
@@ -40,6 +40,11 @@ class ReportsController < ApplicationController
   end
 
   def update
+    @report = Report.find_by(id: params[:id])
+    @report.title = params[:title] if params[:title]
+    @report.body = params[:body] if params[:body]
+    @report.overall_rating = params[:overall_rating] if params[:overall_rating]
+    @report.save
     redirect_to "/reports/#{@report.id}"
   end
 
